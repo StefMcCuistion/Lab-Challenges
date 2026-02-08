f8ab2bd345dfd37937345c706ed5058fe043292b
Stef McCuistion <stef.mccuistion@gmail.com>
2026-02-08 12:20:40 -0600

    Added predict_squareCalculator.py

---
+++ C03/predict_squareCalculator.py
@@ -0,0 +1,15 @@
+print("Square Calculator")
+length = input("Enter the length of the sides in meters: ")
+length = float(length)
+area = length * length
+perimeter = length * 4
+print(f"The area is {round(area, 4)} m^2")
+print(f"The perimeter is {perimeter} m")
+
+# Predict what the code does using comments.
+
+# Run the code to confirm your predictions.
+
+# Investigate:
+
+# Why does the code use float() on line 3?
