3410403343bb6fb8a5a565f52e731eb2b8e5b9c7
Stef McCuistion <stef.mccuistion@gmail.com>
2026-02-08 13:22:53 -0600

    Added modify_squareCalculator.py

---
+++ C03/modify_squareCalculator.py
@@ -0,0 +1,15 @@
+print("Square Calculator")
+length = input("Enter the length of the sides in meters: ")
+length = float(length)
+area = length * length
+perimeter = length * 4
+print(f"The area is {round(area, 4)} m^2")
+print(f"The perimeter is {perimeter} m")
+
+# Modify the above code to:
+
+# 1. Work for a rectangle, by also asking the user for the width.
+
+# 2. Display numbers for area and perimeter so they are rounded to the
+# nearest 2 digits after the decimal point.
+# view rawpyfda_c03_05.py hosted with ❤ by GitHub
