2f1d8cbe61d73ac2afac4f1dc24fbdb106d2efbb
Stef McCuistion <stef.mccuistion@gmail.com>
2026-02-08 13:20:12 -0600

    Corrected inaccurate prediction regarding round()

--- C03/predict_squareCalculator.py
+++ C03/predict_squareCalculator.py
@@ -3,8 +3,8 @@ length = input("Enter the length of the sides in meters: ")
 length = float(length)  # converts string input to float
 area = length * length  # area of square with side length = {length}
 perimeter = length * 4  # perimeter of square with side length = {length}
-print(f"The area is {round(area, 4)} m^2")  # rounds length to nearest
-# multiple of 4
+print(f"The area is {round(area, 4)} m^2")  # rounds to four digits
+# I'd originally guessed that it would round to the nearest multiple of four
 print(f"The perimeter is {perimeter} m")
 
 # Predict what the code does using comments.
