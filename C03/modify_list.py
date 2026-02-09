4def58722156db65c825f78d7340d8468acbc9ac
Stef McCuistion <stef.mccuistion@gmail.com>
2026-02-09 12:22:18 -0600

    Added modify_list.py

---
+++ C03/modify_list.py
@@ -0,0 +1,10 @@
+import random
+
+
+houses = ['Griffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
+my_house = random.choice(houses)
+
+print(f"Sorting Hat: {my_house.upper()}!")
+
+# Modify the code above so the sorting hat, sorts a randomly picked
+# Disney character into a house in Hogwarts.
