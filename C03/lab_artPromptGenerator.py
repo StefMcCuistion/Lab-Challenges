4a04be2bc215c0b342e1c4d20fc4548905893c0d
Stef McCuistion <stef.mccuistion@gmail.com>
2026-02-09 12:30:41 -0600

    Added lab_artPromptGenerator.py

---
+++ C03/lab_artPromptGenerator.py
@@ -0,0 +1,51 @@
+# When artists are trying to keep a daily or regular art practice or
+# challenge, it is hard to start from a blank canvas.
+#
+# We are often more creative when provided with art prompts.
+#
+# Write a art prompt generator for artists.
+# The tool will generate 3 prompts for the user each time it's run.
+#
+# TIP:
+#
+# Your prompt list is going to be rather long. So make sure that you
+# keep your lines under 79 characters while remaining legible with the
+# right indents. A list can be separated into multiple lines as long as
+# it starts with a [ and ends with a ].
+#
+# e.g.
+# greetings = ['hi', 'hello',
+#              'bonjour', 'konnichiwa']
+
+
+# Sample list of 30 art prompts you can use.
+# Clouds
+# Moon
+# Sunflower
+# Stars
+# Planets
+# Emptiness
+# Courage
+# Blues
+# Colorful
+# Hope
+# Diversity
+# Contrast
+# Friendship
+# Shapes
+# Rain
+# Butterfly
+# Heart
+# Flower
+# Freedom
+# Spring
+# Cartoon
+# Layers
+# Patience
+# Soul
+# Sparkles
+# Circles
+# Bubble
+# Mountain
+# Obstacle
+# Affirmation
